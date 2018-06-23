992e9be2696f1cbaf3f542c9e111359dcda406c0
Sergey Kastryulin <snk4tr@gmail.com>
2018-06-23 20:41:46 +0300

    fix(connector): added forgotten connection call

--- run.py
+++ run.py
@@ -6,15 +6,16 @@ from ssh_connector import SshConnector
 def setup():
     config_path = './config.yaml'
     config = yaml.load(open(config_path, 'r'))
-    return config['host'], config['username'], config['password']
+    return config['host'], config['port'], config['username'], config['password']
 
 
-def main(host: str, username: str, password: str):
-    connector = SshConnector(host, username, password)
+def main(host: str, port: str, username: str, password: str):
+    connector = SshConnector(host, port, username, password)
+    connector.connect()
     connector.list_files()
 
 
 if __name__ == "__main__":
-    host, username, password = setup()
+    host, port, username, password = setup()
 
-    main(host, username, password)
+    main(host, port, username, password)
--- ssh_connector.py
+++ ssh_connector.py
@@ -2,21 +2,24 @@ from pssh.clients import ParallelSSHClient
 
 
 class SshConnector:
-    def __init__(self, host, username, password):
+    def __init__(self, host, port, username, password):
         self.host = host
+        self.port = port
         self.username = username
         self.password = password
         self.client = None
 
     def connect(self):
         try:
-            self.client = ParallelSSHClient(hosts=[self.host], user=self.username, password=self.password)
+            self.client = ParallelSSHClient(
+                hosts=[self.host], port=self.port, user=self.username, password=self.password)
 
         except AuthenticationException as e:
-            print('Authentication failure!', e)
+            print('Authentication failure! Credentials must be wrong!', e)
 
     def list_files(self):
-        output = self.client.run_command('ls -la')
+        assert self.client is not None, "Connection object has not been initialized!"
+        output = self.client.run_command('ls')
 
         for host, host_out in output.items():
             for line in host_out.stdout:
