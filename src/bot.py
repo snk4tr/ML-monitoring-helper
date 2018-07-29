817ae24850f8bf80e7a30dcf9bea68311818834b
Sergey Kastryulin <snk4tr@gmail.com>
2018-07-30 00:00:50 +0300

    refactor(telegram-ssh-ml): massive refactoring (watch command probably broken)

--- src/bot.py
+++ src/bot.py
@@ -1,108 +1,111 @@
 import telegram
+import time
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from src.util.text_bulder import construct_gpu_stat_reply, construct_help_reply
 
 
 class Bot:
-    def __init__(self, connector, logger, token):
-        # Init util objects and constants
+    def __init__(self, connector, logger, config):
         self.connector = connector
         self.logger = logger
-        self.WATCH_FREQ = 1  # seconds
+        self.config = config
+        self.mem_thresh = float(self.config.get('mem_thresh'))
+        self.util_thresh = int(self.config.get('util_thresh'))
 
         # Init bot state
         self.watching = False
-        self.learning = False
+        self.is_learning = False
+        self.start_learning_time = None
 
         # Create the EventHandler and pass it your bot's token.
-        updater = Updater(token)
-        job = updater.job_queue
+        self.updater = Updater(config['token'])
+        job = self.updater.job_queue
+        # Get the dispatcher to register handlers.
+        dp = self.updater.dispatcher
+        self.__set_handlers(dp)
 
-        # Get the dispatcher to register handlers
-        dp = updater.dispatcher
+        # Start the Bot
+        self.updater.start_polling()
 
-        # On different commands - answer in Telegram
-        dp.add_handler(CommandHandler("start", self.start))
+        # Run the bot until you press Ctrl-C or the process receives SIGINT.
+        self.updater.idle()
+        self.logger.info("SSH ML bot stops its work! Bye! :)")
+
+    def __set_handlers(self, dp):
+        """On different commands - answer in Telegram."""
         dp.add_handler(CommandHandler("help", self.help))
         dp.add_handler(CommandHandler('ls', self.list_files))
         dp.add_handler(CommandHandler('gpu', self.get_gpu_stat))
         dp.add_handler(CommandHandler('watch', self.watch_learning, pass_job_queue=True))
-
+        dp.add_handler(CommandHandler('stop', self.stop_bot))
         dp.add_handler(MessageHandler(Filters.command, self.unknown))
-
-        # Log all errors
         dp.add_error_handler(self.error)
-
-        # Start the Bot
-        updater.start_polling()
-
-        # Run the bot until you press Ctrl-C or the process receives SIGINT,
-        try:
-            updater.idle()
-        except KeyboardInterrupt:
-            self.logger.info("SSH ML bot stops its work! Bye! :)")
-        finally:
-            self.finish_work()
-
-    def start(self, bot, update):
-        """Send a message when the command /start is issued."""
-        update.message.reply_text('Hi!')
 
     def help(self, bot, update):
         """Send helping information about how the bot works when the command /help is issued."""
         chat_id = update.message.chat_id
-        reply = 'There are a few pretty useful commands for you to try:\n\n/gpu - provides *gpustat*-like info about ' \
-                'current work machine load.\n/ls - performs plain old *ls* command (lists files in current directory).'
-        bot.send_message(chat_id=chat_id,
-                         text=reply,
-                         parse_mode=telegram.ParseMode.MARKDOWN)
+        reply = construct_help_reply()
+        bot.send_message(chat_id=chat_id, text=reply, parse_mode=telegram.ParseMode.MARKDOWN)
 
     def error(self, update, error):
         """Log Errors caused by Updates."""
         self.logger.warning('Update "%s" caused error "%s"', update, error)
 
     def unknown(self, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+        bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command 🆘")
 
     def list_files(self, bot, update):
-        """List files in current directory"""
+        """List files in current directory."""
         reply = self.connector.list_files()
         update.message.reply_text(reply)
 
     def get_gpu_stat(self, bot, update):
         """Provide detailed information about current GPU usage."""
         chat_id = update.message.chat_id
-        degree_sign = u'\N{DEGREE SIGN}'
-        name, util, usedm, totalm, temp = self.connector.get_gpu_stat()
-        reply = ''
-        reply += '`' + str(name) + '`\n'
-        reply += '*Utilization*: ' + str(util) + ' / ' + '100%\n'
-        reply += '*Memory*: ' + str(usedm) + ' / ' + str(totalm) + ' MB\n'
-        reply += '*Temperature*: `' + str(temp) + degree_sign + 'C`'
+        reply = construct_gpu_stat_reply(self.connector)
         bot.send_message(chat_id=chat_id, text=reply, parse_mode=telegram.ParseMode.MARKDOWN)
 
     def watch_learning(self, bot, update, job_queue):
+        """Start/stop monitoring or learning on the remote machine."""
         chat_id = update.message.chat_id
         self.watching = not self.watching
         if self.watching:
-            bot.send_message(chat_id=chat_id, text='Monitoring of learning has been started!')
-            job_queue.run_repeating(self.__watch, self.WATCH_FREQ, context=update.message.chat_id)
+            bot.send_message(chat_id=chat_id, text='Monitoring of learning has been started!📡')
+            job_queue.run_repeating(self.__watch, self.config.get('watch_freq'), context=update.message.chat_id)
             return
-        # Temporarily disable monitoring
-        bot.send_message(chat_id=chat_id, text='Monitoring of learning has been stopped!')
+        bot.send_message(chat_id=chat_id, text='Monitoring of learning has been stopped❗️❌')
         job_queue.enabled = self.watching
 
     def __watch(self, bot, job):
+        """Periodically check whether learning has been started/stopped."""
         _, util, usedm, totalm, _ = self.connector.get_gpu_stat()
-        mem_thresh = usedm / totalm
-        if not self.learning and mem_thresh > 0.9 and util > 10:
-            self.learning = not self.learning
-            bot.send_message(chat_id=job.context, text='Learning started!')
+        mem_usage = usedm / totalm
+        if self.__learning_stopped(mem_usage, util):
+            self.__start_learning(bot, job)
+        if not self.__learning_stopped(mem_usage, util):
+            if not self.is_learning:
+                return
+            self.__stop_learning(bot, job)
 
-        if self.learning and mem_thresh < 0.9 and util < 10:
-            self.learning = not self.learning
-            bot.send_message(chat_id=job.context, text='Learning finished!')
+    def __learning_stopped(self, mem_usage, util):
+        return not self.is_learning and mem_usage > self.mem_thresh and util > self.util_thresh
 
-    def finish_work(self, bot, update):
+    def __stop_learning(self, bot, job):
+        self.is_learning = not self.is_learning
+        secs = time.time() - self.start_learning_time
+        hrs, mns, secs = int(secs // 3600), (int(secs // 60)) % 60, int(secs % 60)
+        text = f'🔚 Learning finished!🎉  It took *{hrs} hours {mns} minutes {secs} seconds*❗️'
+        bot.send_message(chat_id=job.context, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
+
+    def __start_learning(self, bot, job):
+        self.is_learning = not self.is_learning
+        bot.send_message(chat_id=job.context, text='Learning started❗️🔜🙏')
+        self.start_learning_time = time.time()
+
+    def stop_bot(self, bot, update):
+        """Make bot inactive. The program will still be running though"""
         chat_id = update.message.chat_id
-        bot.send_message(chat_id=chat_id, text='Bye!', parse_mode=telegram.ParseMode.MARKDOWN)
+        text = 'Bot is finishing its work. See ya! 😏'
+        bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
+        self.updater.stop()
